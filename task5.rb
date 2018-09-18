06eea8e82b8585e625f5dd5043c84c6052731f55
alexeybokov <lexabokov18@gmail.com>
2018-09-18 15:11:36 +0300

    complete task

---
+++ task5.rb
@@ -0,0 +1,18 @@
+class Task5
+  def formatize(str, action)
+    case action
+    when :cap_words
+      str.split(' ').map { |w| w.capitalize }.join('')
+    when :downcase
+      str.split(' ').map { |w| w.downcase }.join('_')
+    when :css_words
+      str.split(' ').map { |w| w }.join('-')
+    end
+  end
+
+  str = 'i love ruby'
+
+  p formatize(str, :cap_words)
+  p formatize(str, :downcase)
+  p formatize(str, :css_words)
+end
