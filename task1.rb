facfa6b07ed18d02485c87d819aa739b1c539e8b
alexeybokov <lexabokov18@gmail.com>
2018-09-19 10:01:24 +0300

    refactor Task1

--- task1.rb
+++ task1.rb
@@ -1,12 +1,3 @@
 class Task1
-  for i in 1..30
-    if i == 5
-      puts "ruby"
-    elsif i == 20
-      puts "ruby"
-    elsif i == 25
-      puts "ruby"
-    end
-    puts "<3 ruby"
-  end
+  30.times { |i| [5, 20, 25].include?(i+1) ? puts('ruby') : puts('<3ruby') }
 end
