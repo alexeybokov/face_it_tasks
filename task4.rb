ca817ba06024123fdec9c4ac8d85117fc7fa9d41
alexeybokov <lexabokov18@gmail.com>
2018-09-19 10:24:35 +0300

    refactor Task4

--- task4.rb
+++ task4.rb
@@ -2,16 +2,15 @@
 
 class Task4
   def nested_hash_value(obj,key)
-    if obj.respond_to?(:key?) && obj.key?(key)
-      obj[key]
-    elsif obj.respond_to?(:each)
-      r = nil
-      obj.find{ |*a| r=nested_hash_value(a.last,key) }
-      r
+    obj.each do |k, v|
+      if v.is_a?(Hash)
+        nested_hash_value(v, key)
+      elsif k == key.to_sym
+        puts v.inspect
+      end
     end
   end
-
-  hash = { key1: {}, key2: {}, key3: { key4: 'str', key5: 'str2', key6: { key7: { key8: 1, key9: [2]} } }}
-
-  p nested_hash_value(hash, :key9)
 end
+
+hash = { key1: {}, key2: {}, key3: { key4: 'str', key5: 'str2', key6: { key7: { key8: 1, key9: [2]} } }}
+Task4.new.nested_hash_value(hash, :key9)
