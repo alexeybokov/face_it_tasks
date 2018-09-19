d22d7fbcb57d93760d2d6649ad1dbc331a702a75
alexeybokov <lexabokov18@gmail.com>
2018-09-19 10:38:42 +0300

    refactor Task6

--- task6.rb
+++ task6.rb
@@ -8,6 +8,5 @@ end
 
 array = [[1, 2, 3, 4, '1'], ['2', '5', '10'], [111, 222, 333, 444], ['i', 'love', 'ruby'], { key: 'value' }, [[['text', 100_000]]]]
 
-
 p Task6.new.get_all(array, String)
 p Task6.new.get_all(array, Integer)
